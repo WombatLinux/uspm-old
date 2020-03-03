7fb96bfc63bd2c7b41c2ee8619366c6eb18f07f3
afroraydude <thomasray0@gmail.com>
2020-03-03 15:13:31 -0500

    another fix

--- uspm-mkpkg/main.c
+++ uspm-mkpkg/main.c
@@ -18,15 +18,6 @@ int main(int argc, char *argv[]) {
     createpkg();
 }
 
-char* concat(const char *s1, const char *s2)
-{
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
-}
-
 void write_file(char *filename, char *out) {
     char *outfile = concat(out, "\n");
 
